386a30b29fc293c12936af1edb57e53d0c61c8a8
mrmapleleaf <kaple.de.96@gmail.com>
2024-03-06 23:42:34 +0900

    タスク一覧をコンポーネント化

---
+++ components/taskListContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>タスク一覧ですよ</h2>
+  <div class="item">
+    <div v-for="[id, task] in taskList">
+      {{ id }}：
+      <NuxtLink v-bind:to="{name: 'task-taskDetail-id', params:{id: id}}">
+        {{ task.title }}
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { task } from '~/interfaces';
+const taskList = useState<Map<number, task>>('taskList');
+</script>
+
+<style scoped>
+  .item {
+    padding: 10px;
+  }
+</style>
--- pages/task/taskList.vue
+++ pages/task/taskList.vue
@@ -1,21 +1,11 @@
 <template>
   <h1>タスク一覧画面ですよ</h1>
-  <div>
-    <h2>タスク一覧ですよ</h2>
-      <ul>
-        <template v-for="[id, task] in taskList">
-          <NuxtLink v-bind:to="{name: 'task-taskDetail-id', params: {id: id}}">
-            <li>{{ id }} : {{ task.title }}</li>
-          </NuxtLink>
-        </template>
-      </ul>
-  </div>
+  <taskListContainer>
+  </taskListContainer>
   <NuxtLink v-bind:to="{name: 'task-taskAdd'}">タスク追加画面に遷移しますよ</NuxtLink><br>
   <NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink>
 </template>
 
 <script setup lang="ts">
-import type { task } from '~/interfaces';  
-
-const taskList = useState<Map<number, task>>('taskList');
+import taskListContainer from '~/components/taskListContainer.vue';
 </script>

f349934da7dfafa10e89be51c0641df193c373be
mrmapleleaf <kaple.de.96@gmail.com>
2024-04-02 00:00:44 +0900

    警告の方法を変更

--- components/taskForm.vue
+++ components/taskForm.vue
@@ -1,40 +1,45 @@
 <template>
-<div class="taskForm">
-    <form v-on:submit.prevent="onAddTask"> 
+  <div class="taskForm">
+    <form v-on:submit.prevent="onAddTask">
       <dl>
         <dt>
           <label for="id">id&nbsp;</label>
         </dt>
         <dd>
-          <input type="number" id="id" v-model.number.lazy='newTask.id' required>
+          <input
+            type="number"
+            id="id"
+            v-model.number.lazy="newTask.id"
+            required
+          />
         </dd>
         <dt>
           <label for="title">タスク名&nbsp;</label>
         </dt>
         <dd>
-          <input type="text" id="title" v-model.lazy='newTask.title' required>
+          <input type="text" id="title" v-model.lazy="newTask.title" required />
         </dd>
         <dt>
           <label for="detail">タスク詳細&nbsp;</label>
         </dt>
         <dd>
-          <textarea id="detail" v-model.lazy='newTask.detail'></textarea>
+          <textarea id="detail" v-model.lazy="newTask.detail"></textarea>
         </dd>
         <dt>
           <label for="note">備考&nbsp;</label>
         </dt>
         <dd>
-          <textarea id="note" v-model.lazy='newTask.note'></textarea>
+          <textarea id="note" v-model.lazy="newTask.note"></textarea>
         </dd>
       </dl>
       <button type="submit">登録</button>
     </form>
   </div>
-  <NuxtLink v-bind:to="{name: 'task-taskList'}">戻る</NuxtLink>
+  <NuxtLink v-bind:to="{ name: 'task-taskList' }">戻る</NuxtLink>
 </template>
 
 <script setup lang="ts">
-import type { task } from '~/interfaces.ts'
+import type { task } from '~/interfaces.ts';
 
 const state = useState<Map<number, task>>('taskList');
 const router = useRouter();
@@ -44,30 +49,27 @@ const newTask: task = reactive({
   title: '',
   detail: '',
   note: '',
-  completed: false
+  completed: false,
 });
 console.log(newTask);
 
 const onAddTask = (): void => {
-
-  if(!state.value.has(newTask.id)) {
+  if (!state.value.has(newTask.id)) {
     state.value.set(newTask.id, newTask);
-    router.push({name: 'task-taskList'});
+    router.push({ name: 'task-taskList' });
   } else {
-    console.log('入力されたIDは既に存在しています。')
+    alert('入力されたIDは既に存在しています。');
   }
-  
-}
+};
 
 watch(newTask, () => {
   console.log(newTask);
-})
-
+});
 </script>
 
 <style scoped>
-  .taskForm {
-    border: black 1px solid;
-    padding: 10px;
-  }
+.taskForm {
+  border: black 1px solid;
+  padding: 10px;
+}
 </style>

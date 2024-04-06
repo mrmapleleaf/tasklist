19b2df78e20e147b65a2b6271c78ecab1d1e5080
mrmapleleaf <kaple.de.96@gmail.com>
2024-04-06 17:53:25 +0900

    タスク一覧をAPIAPIから取得するように変更

--- components/taskListContainer.vue
+++ components/taskListContainer.vue
@@ -1,22 +1,57 @@
 <template>
   <h2>タスク一覧ですよ</h2>
   <div class="item">
-    <div v-for="[id, task] in taskList">
-      {{ id }}：
-      <NuxtLink v-bind:to="{name: 'task-taskDetail-id', params:{id: id}}">
-        {{ task.title }}
-      </NuxtLink>
-    </div>
+    <template v-if="pending">
+      <p>データ取得中</p>
+    </template>
+    <template v-else>
+      <div class="taskList-wrapper">
+        <div
+          v-for="(task, index) in taskList"
+          v-bind:key="`key-${index}`"
+          class="task"
+        >
+          <p>{{ task.id }}</p>
+          <NuxtLink
+            v-bind:to="{
+              name: 'task-taskDetail-id',
+              params: { id: task.id },
+            }"
+          >
+            <span>{{ task.title }}</span>
+          </NuxtLink>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { task } from '~/interfaces';
-const taskList = useState<Map<number, task>>('taskList');
+
+const pending = ref(false);
+const error = ref('');
+const taskList = ref([] as Array<task>);
+
+onMounted(() => {
+  getTaskList();
+});
+
+const getTaskList = async () => {
+  try {
+    pending.value = true;
+    taskList.value = await $fetch('http://localhost:8080/task/list');
+  } catch (e) {
+    console.error('APIの呼び出しでエラーが発生しました', e);
+    error.value = 'APIの呼び出しでエラーが発生しました';
+  } finally {
+    pending.value = false;
+  }
+};
 </script>
 
 <style scoped>
-  .item {
-    padding: 5px 10px 20px 10px;
-  }
+.item {
+  padding: 5px 10px 20px 10px;
+}
 </style>
